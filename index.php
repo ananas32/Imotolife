4e9782eeee9796638463041ecf28bfe149f3b69c
anans32 <porsche839@gmail.com>
2018-02-20 20:35:45 +0200

    add script sent to email

--- index.php
+++ index.php
@@ -1,5 +1,46 @@
+<?php
+//copy paste form http://www.freecontactform.com/email_form.php
+if (isset($_POST['name'])) {
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+    $email_to = "romantarasovich@gmail.com";
+    $email_subject = "Your email subject line";
+
+    function died()
+    {
+        echo "Нажаль сталася помилка при відправці форми, поверніться будь-ласка і перевірте валідність даних";
+        die();
+    }
+
+    if (empty($_POST['name']) && empty($_POST['text'])) {
+        died();
+    }
+
+    $name = $_POST['name']; // required
+    $email = $_POST['email'];
+    $text = $_POST['text']; // required
+
+    $email_message = "Form details below.\n\n";
+
+
+    function clean_string($string)
+    {
+        $bad = array("content-type", "bcc:", "to:", "cc:", "href");
+        return str_replace($bad, "", $string);
+    }
+
+    $email_message .= "Ім'я: " . clean_string($name) . "\n";
+    $email_message .= "Email: " . clean_string($email) . "\n";
+    $email_message .= "Текст: " . clean_string($text) . "\n";
+
+    // create email headers
+    $headers = 'From: ' . 'mail@imoto.life' . "\r\n" .
+        'Reply-To: ' . 'mail@imoto.life' . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+    @mail($email_to, $email_subject, $email_message, $headers);
+}
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 
 <head>
 
@@ -34,16 +75,19 @@
         <div class="row h-100">
             <div class="col-12 my-auto">
                 <div class="masthead-content text-white py-5 py-md-0">
+                    <?if (isset($_POST['name'])) {
+                        echo '<div class="alert alert-success"><strong>Дані успішно відправлені</strong></div>';
+                    }?>
                     <h1 class="mb-3">Мотоцикли!</h1>
                     <p class="mb-5">Допоможіть нам зробити краще, но це не точна
-                    <form action="">
-                        <input type="email" class="form-control" placeholder="Ваше ім'я">
+                    <form action="/" method="POST">
+                        <input type="text" name="name" class="form-control" placeholder="Ваше ім'я" required="required">
                         <br>
-                        <input type="email" class="form-control" placeholder="Ваш email">
+                        <input type="text" name="email" class="form-control" placeholder="Ваш email">
                         <br>
-                        <textarea class="form-control" placeholder="Побажання"></textarea>
+                        <textarea name="text" class="form-control" placeholder="Побажання" rows="4" required="required"></textarea>
                         <br>
-                        <button class="btn btn-secondary" type="button">Відправити побажання</button>
+                        <button class="btn btn-secondary" type="submit">Відправити побажання</button>
                     </form>
                 </div>
             </div>
